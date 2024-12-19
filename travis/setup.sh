3a2252578016833e4b7420f8250a8117cd28a858
Serhii Kumeskyi <serhiikumeskyii@MBP-van-Serhii.home>
2024-12-19 16:41:07 +0100

    yml fix with sf 2

--- travis/setup.sh
+++ travis/setup.sh
@@ -1,3 +1,4 @@
 echo "running setup process for ${{ github.repository }} 2"
 sf org login access-token --instance-url ${{LOGIN_URL}} --no-prompt
+sf sgd:source:delta --to "HEAD" --from main --output .
 echo "echoing setup process ran successfully 2"
