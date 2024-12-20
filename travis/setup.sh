84fb8b1f7c2f561bbd5e7261d0c63b8806dc7819
Serhii Kumeskyi <serhiikumeskyii@MBP-van-Serhii.home>
2024-12-20 14:17:26 +0100

    with real yml 1

--- travis/setup.sh
+++ travis/setup.sh
@@ -1,4 +1,4 @@
 echo "running setup process for ${{ github.repository }} "
 #sf org login access-token --instance-url https://login.salesforce.com --no-prompt
-sf data query --query "SELECT Id, Name FROM Account LIMIT 2" -o 00DWU00000CvjrA!AQEAQCbGr2.zorkibrHYjqMLo0yRoMcbeXkcvAygKz3fFabneRT.UQU_ZAfExqESQEpCuzJRyDjTsl1flkBKCc7uAF09pVij
+sf data query --query "SELECT Id, Name FROM Account LIMIT 3"
 echo "echoing setup process ran successfully"
