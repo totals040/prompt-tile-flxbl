3b1120003ba54e538dbc689129ccf58d0599ce62
Serhii Kumeskyi <serhiikumeskyii@MBP-van-Serhii.home>
2024-12-24 10:06:39 +0100

    with merge 1

--- travis/setup.sh
+++
@@ -1,4 +0,0 @@
-echo "running setup process for ${{ github.repository }} "
-#sf org login access-token --instance-url https://login.salesforce.com --no-prompt
-sf data query --query "SELECT Id, Name FROM Account LIMIT 3"
-echo "echoing setup process ran successfully"
